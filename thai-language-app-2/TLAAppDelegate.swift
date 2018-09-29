4038720fef325677893c67c524cdc2f2231226e4
Jonathan Winger Lang <jontelang@gmail.com>
2018-09-29 12:06:56 +0700

    Remove the shadow on the navigation bar

--- thai-language-app-2/TLAAppDelegate.swift
+++ thai-language-app-2/TLAAppDelegate.swift
@@ -43,6 +43,12 @@ class TLAAppDelegate: UIResponder, UIApplicationDelegate {
         // Set the tintcolor as a default for items
         window?.tintColor = UIColor.appTintColor()
         
+        // Removes the shadow. The 'setShadowImage' doesn't work unless we also set
+        // the backgroundImage to an empty image. For one reasons or another.
+        let navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.setBackgroundImage(UIImage.init(), for: UIBarMetrics.default)
+        navigationBarAppearance.shadowImage = UIImage.init()
+        
         // Nice rounded edges on the whole app. At least on non iPhone X devices
         window?.layer.cornerRadius = 4.0
         window?.layer.masksToBounds = true
