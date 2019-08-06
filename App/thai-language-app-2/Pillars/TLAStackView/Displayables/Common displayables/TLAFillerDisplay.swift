beb080f55a1fc2f421cbfd9ec26c637dffc473af
Jonathan Winger Lang <jontelang@gmail.com>
2019-08-06 22:37:51 +0700

    Add documentation for TLAFillterDisplay

--- App/thai-language-app-2/Pillars/TLAStackView/Displayables/Common displayables/TLAFillerDisplay.swift
+++ App/thai-language-app-2/Pillars/TLAStackView/Displayables/Common displayables/TLAFillerDisplay.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+/// Purpose: Acts as an empty view that you can add into a TLADisplayRow to act
+/// as a spacer. E.g. if you want a layout like this:
+///
+///   [a][b][c]
+///   [d][e][ ] <-- Filler to preserve the grid
+///
+/// Without a filler at the end, the default behaviour of the stackview would be
+///
+///   [a][b][c]
+///   [d][eeee]
+///
+/// Which is probably not what you want.
 struct TLAFillerDisplay: TLAStackViewDisplayable {
     func view() -> UIView {
         let view = UIView()
