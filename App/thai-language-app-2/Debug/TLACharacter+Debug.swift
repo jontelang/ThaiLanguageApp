dfa0b7d400b7968b48fd71758aa080b7fcd53d3b
Jonathan Winger Lang <jontelang@gmail.com>
2019-08-12 18:43:05 +0700

    Add TLACharacter debug convenience init

---
+++ App/thai-language-app-2/Debug/TLACharacter+Debug.swift
@@ -0,0 +1,28 @@
+//
+//  TLACharacter+Debug.swift
+//  thai-language-app-2
+//
+//  Created by Jonathan on 8/12/19.
+//  Copyright © 2019 Jonathan Winger Lang. All rights reserved.
+//
+
+import Foundation
+
+/// Purpose: Convenience initialisers
+extension TLACharacter {
+    init() {
+        identifier = "character_identifier"
+        thaiCharacter = "ก"
+        thaiNameInEnglish = "Ko Kai"
+        thaiNameInThai = "ก ไก่"
+        alternativeThaiCharacter = "alt ก"
+        type = .consonant
+        characterHeight = .normal
+        toneClass = "mid"
+        pronounciation = "gaw gai"
+        pronounciationComment = "this is just a test class"
+        soundStart = "g-"
+        soundEnd = "-k"
+        vowelType = "not a vowel"
+    }
+}
